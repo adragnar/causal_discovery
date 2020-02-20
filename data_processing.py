a34a31faa3b3a63177e82ec7a8015e2b91e6b0bc
Robert Adragna <robert.adragna1381@gmail.com>
2020-02-19 21:30:42 -0500

    Generalized data extraction process

--- data_processing.py
+++ data_processing.py
@@ -19,22 +19,66 @@ from torch.utils.data import DataLoader
 
 #import matplotlib.pyplot as plt
 
-def adult_dataset_processing(fname, fteng):
-    '''Process the adult dataset from csv. Return the dataframe, as well as a
-    list of columns that must be treated as one block during the enumeration of plausible causal predictors
+def data_conversion(data, categorical_feats, continous_feats, predictor_feats, fteng):
+    '''
     
-    fteng: List of the feature engineering steps to do on data. 
-    1) Introtuce products of cat + cont vars
-    2) Introduce square of cont vars 
-    3) Convert education to continous
-    :return: cleaned dataframe with info
+    :param data: Dataframe of cleaned data 
+    :param categorical_feats: categorical feats (all including labels)
+    :param continous_feats: (all including labels)
+    :param predictor_feats: all
+    :param fteng: ids of feature manipulations wanted
+    :return: 
     '''
 
+    # Categorical to one-hot
+    data = pd.get_dummies(data, columns=categorical_feats, drop_first=True)
+    #data['env_select'] = 1  #Use this column for env selection
+
+    #Feature engineering
+    orig_cols = data.columns  # So that no problems with adding feats
+
+    if 1 in fteng:  #x^2 continous feats
+        for col in orig_cols:
+            if col in continous_feats:  #Assume only transform orig cont col
+                data[(col + '_sq')] = data[col] ** 2
+
+    if 2 in fteng:  #add new col with multiplied feats
+            for i, cp in enumerate(combinations(orig_cols, 2)):
+                data[(cp[0] + '_x_' + cp[1])] = data[cp[0]] * data[cp[1]]
+
+    #Note - doing a logical or of features will break dummy encoding
+
+
+    #Get the unmodified attribute classes for possible envs
+    all_cats={cat:[] for cat in ([ft for ft in (categorical_feats + continous_feats) if ft not in predictor_feats])}
+
+    for col in data.columns:
+        for cat in all_cats:
+            if (cat == col) or (((cat+'_') in col) and ('_sq' not in col) \
+                                        and ('_x_' not in col)):
+                all_cats[cat].append(col)
+    for cat in all_cats:  #Dummy case
+        all_cats[cat].append(cat + '_DUMmY')
+
+    print(data.shape)
+    return data, all_cats
+
+def adult_dataset_processing(fname, fteng):
+    '''Process the adult dataset from csv. Return the dataframe, as well as a
+        list of columns that must be treated as one block during the enumeration of plausible causal predictors
+
+        fteng: List of the feature engineering steps to do on data. 
+        1) Introtuce products of cat + cont vars
+        2) Introduce square of cont vars 
+        3) Convert education to continous
+        :return: cleaned dataframe with info
+        '''
+
     """ Adult income classification
-    
+
     In this lab we will build our own neural network pipeline to do classification on the adult income dataset. More
     information on the dataset can be found here: http://www.cs.toronto.edu/~delve/data/adult/adultDetail.html
-    
+
     """
     seed = 0
 
@@ -73,55 +117,73 @@ def adult_dataset_processing(fname, fteng):
     # Hint: take a look at what data[data["income"] != ">50K"] returns
     # Hint: if data[field] do not contain strings then data["income"] != ">50K" will return an error
 
-        ######
+    ######
 
-        # 2.3 YOUR CODE HERE
+    # 2.3 YOUR CODE HERE
     for feature in col_names:
         try:
             data = data.loc[data[str(feature)] != "?"]
-        except:  #If the column is all numbers and str comparisons dont work
+        except:  # If the column is all numbers and str comparisons dont work
             pass
-        ######
+            ######
 
-
-    categorical_feats = ['workclass', 'education', 'marital-status', \
-                         'occupation', 'relationship', 'race', 'sex', \
-                         'native-country', 'income', ]
-    continous_feats = ['age', 'fnlwgt', 'educational-num', 'capital-gain', \
+    #NOTE - These lists aren't MECE
+    cat_feats = ['workclass', 'education', 'marital-status', \
+                         'occupation', 'relationship', 'race', 'gender', \
+                         'native-country', 'income']
+    cont_feats = ['age', 'fnlwgt', 'educational-num', 'capital-gain', \
                        'capital-loss', 'hours-per-week']
+    pred_feats = ['income']
 
-    # Categorical to one-hot
-    data = pd.get_dummies(data, prefix=categorical_feats, drop_first=True)
-    #data['env_select'] = 1  #Use this column for env selection
-
-    #Feature engineering
-    orig_cols = data.columns  # So that no problems with adding feats
-
-    if 1 in fteng:  #x^2 continous feats
-        for col in orig_cols:
-            if col in continous_feats:  #Assume only transform orig cont col
-                data[(col + '_sq')] = data[col] ** 2
-
-    if 2 in fteng:  #add new col with multiplied feats
-            for cp in combinations(orig_cols, 2):
-                data[(cp[0] + '_x_' + cp[1])] = data[cp[0]] * data[cp[1]]
-
-    #Note - doing a logical or of features will break dummy encoding
+    return data_conversion(data, cat_feats, cont_feats, pred_feats, fteng)
 
 
-    #Get the unmodified attribute classes for possible envs
-    all_cats = {'age':[], 'workclass':[], 'fnlwgt':[], 'education':[], 'educational-num':[], 'marital-status':[], \
-                'occupation':[], 'relationship':[], \
-     'race':[], 'sex':[], 'capital-gain':[], 'capital-loss':[], 'hours-per-week':[], 'native-country':[]}
+# def german_credit_dataset_processing(fname, fteng=[]):
+#     data = pd.read_csv(fname)
+#     data = data.drop('Telephone', axis=1)
+#
+#     categorical_feats = ['Purpose', 'Savings', 'Personal', 'OtherDebtors', \
+#                          'Property', 'OtherInstallmentPlans', 'Housing', \
+#                          'Foreign', 'Labels']
+#     integer_feats = ['CheqAccountStatus', 'Duration', 'CreditHistory', \
+#                      'PresentEmployment', 'Age', 'Job', 'Deps' ]
+#     continous_feats = ['CreditAmount', 'InstallmentDisposable', 'PresRes', \
+#                        'NumExistCredits', ]
+#
+#     in_order = ['Labels', 'CheqAccountStatus', 'Duration', 'CreditHistory', 'Purpose', \
+#                 'CreditAmount', 'Savings', 'PresentEmployment', \
+#                 'InstallmentDisposable', 'Personal', 'OtherDebtors', 'PresRes', \
+#                 'Property', 'Age', 'OtherInstallmentPlans', 'Housing', \
+#                 'NumExistCredits', 'Job', 'Deps', 'Foreign']
+#
+#     # Note - for knowing which column is which, might be good to associate numbers with labels
+#     data.columns = in_order
+#     data = pd.get_dummies(data, columns=categorical_feats, drop_first=True)
+#
+#
+#     #Feature engineering
+#     orig_cols = data.columns  # So that no problems with adding feats
+#
+#     if 1 in fteng:  #x^2 continous feats
+#         for col in orig_cols:
+#             if col in (continous_feats + integer_feats):  #Assume only transform orig cont col
+#                 data[(col + '_sq')] = data[col] ** 2
+#
+#     if 2 in fteng:  #add new col with multiplied feats
+#             for cp in combinations(orig_cols, 2):
+#                 data[(cp[0] + '_x_' + cp[1])] = data[cp[0]] * data[cp[1]]
 
-    for col in data.columns:
-        for cat in all_cats:
-            if (cat == col) or (((cat+'_') in col) and ('_sq' not in col) \
-                                        and ('_x_' not in col)):
-                all_cats[cat].append(col)
-    for cat in all_cats:  #Dummy case
-        all_cats[cat].append(cat + '_DUMmY')
 
-    print(data.shape)
-    return data, all_cats
 
+
+if __name__ == '__main__':
+    # german_credit_dataset_processing('data/german_credit.csv')
+    a, __ = adult_dataset_processing('data/adult.csv', [])
+    b, __ = new_adult_dataset_processing('data/adult.csv', [])
+
+    assert a.equals(b)
+    # print(len(a.columns), len(b.columns))
+    # assert a.columns == b.columns
+    # for col in a.columns:
+    #     print(col)
+    #     assert a[col].equals(b[col])
