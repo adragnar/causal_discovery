475bb4519e6bed8390a0b6491efcfd3f33d084a3
Robert Adragna <robert.adragna1381@gmail.com>
2020-06-12 12:23:08 -0400

    Added critical file for running experiments I forgot to commit earlier

---
+++ algo_hyperparams.py
@@ -0,0 +1,43 @@
+from utils import dname_from_fpath
+
+def get_irm_args(dset_fname):
+    if dname_from_fpath(dset_fname) == 'adult':
+        args =  {'lr': 0.01, \
+                 'n_iterations':3000, \
+                 'penalty_anneal_iters':500, \
+                 'l2_reg':0.0001, \
+                 'pen_wgt':3000, \
+                 'hid_layers':200, \
+                 'verbose':True}
+
+    elif dname_from_fpath(dset_fname) == 'german':
+        args =  {'lr':0.0001, \
+                 'n_iterations':2000, \
+                 'penalty_anneal_iters':100, \
+                 'l2_reg':0.001, \
+                 'pen_wgt':3000, \
+                 'hid_layers':200, \
+                 'verbose':True}
+
+    else:
+        assert False
+
+    return args
+
+def get_linear_irm_args(dset_fname):
+    args =  {'lr': 0.01, \
+             'n_iterations':1000, \
+             'penalty_anneal_iters':1, \
+             'l2_reg':0.0, \
+             'pen_wgt':1000, \
+             'hid_layers':100, \
+             'verbose':True}
+    return args
+
+def get_linreg_args(dset_fname):
+    args =  {'lambda':1e-5}
+    return args
+
+def get_logreg_args(dset_fname):
+    args =  {'C':0.001}
+    return args
